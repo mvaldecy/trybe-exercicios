375eb9246ef22de253b6ad11ae7b7f42e72c60d1
Marcos Valdecy <mvaldecy11@gmail.com>
2021-07-29 19:04:20 -0300

    4ª questão

--- Fundamentos/bloco-4-intro-javascript/dia-2-array-loop-for/script.js
+++ Fundamentos/bloco-4-intro-javascript/dia-2-array-loop-for/script.js
@@ -19,11 +19,24 @@ for (let i=0;i<numbers.length;i++){
     soma += numbers[i]
 }
 console.log(soma)
-/*Para o terceiro exercício, calcule e imprima 
+/* 3)Para o terceiro exercício, calcule e imprima 
 a média aritmética dos valores contidos no array;*/
 console.log(separador)
 console.log('3ª questão')
 let media =soma/numbers.length
 console.log (media)
+/* 4)Com o mesmo código do exercício anterior, 
+caso o valor final seja maior que 20, 
+imprima a mensagem: "valor maior que 20".
+Caso não seja, imprima a mensagem: 
+"valor menor ou igual a 20";*/
+console.log(separador)
+console.log('4ª questão')
+if (media > 20){
+    console.log('valor maior que 20')
+}
+else {
+    console.log('valor menor que 20')
+}
 
 
