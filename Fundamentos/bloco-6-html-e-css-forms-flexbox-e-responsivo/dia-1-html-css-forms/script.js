b35467352dae84b2868618cccad2edc5c1fc69c3
Marcos Valdecy <mvaldecy11@gmail.com>
2021-08-30 15:36:15 -0300

    final

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,41 @@
+let estados = [
+    'AC - Acre',
+    'AL - Alagoas',
+    'AP - Amapá',
+    'AM - Amazonas',
+    'BA - Bahia',
+    'CE - Ceará',
+    'DF - Distrito Federal',
+    'ES - Espírito Santo',
+    'GO - Goías',
+    'MA - Maranhão',
+    'MT - Mato Grosso',
+    'MS - Mato Grosso do Sul',
+    'MG - Minas Gerais',
+    'PA - Pará',
+    'PB - Paraíba',
+    'PR - Paraná',
+    'PE - Pernambuco',
+    'PI - Piauí',
+    'RJ - Rio de Janeiro',
+    'RN - Rio Grande do Norte',
+    'RS - Rio Grande do Sul',
+    'RO - Rondônia',
+    'RR - Roraíma',
+    'SC - Santa Catarina',
+    'SP - São Paulo',
+    'SE - Sergipe',
+    'TO - Tocantins'
+    ];
+
+    let select = document.querySelector('select')
+    console.log(select)
+    
+    for (let i = 0; i < estados.length; i++) {
+        let option = document.createElement('option')
+        option.innerHTML = estados[i]  
+        select.appendChild(option)
+
+
+    }
+ 
