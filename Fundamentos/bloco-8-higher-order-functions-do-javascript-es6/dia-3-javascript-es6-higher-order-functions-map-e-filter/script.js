6d4a49a2dad995bc629ae724943c969c929300c7
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-09 15:05:15 -0300

    2ª questao

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -83,4 +83,47 @@ const expectedResult = [
   
    assert.deepStrictEqual(formatedBookNames(), expectedResult);
 
+   const expectedResult2 = [
+    {
+      age: 31,
+      author: 'Isaac Asimov',
+    },
+    {
+      age: 38,
+      author: 'H. P. Lovecraft',
+    },
+    {
+      age: 39,
+      author: 'Stephen King',
+    },
+    {
+      age: 43,
+      author: 'George R. R. Martin',
+    },
+    {
+      age: 45,
+      author: 'Frank Herbert',
+    },
+    {
+      age: 62,
+      author: 'J. R. R. Tolkien',
+    },
+  ];
+  
+  function nameAndAge() {
+    // escreva seu código aqui
+    return books
+    .map ((book) => (
+        {
+            author: book.author.name,
+            age: book.releaseYear - book.author.birthYear,
+        }
+    ))
+    .sort((obj1, obj2) => obj1.age - obj2.age);
+  }
+  
+  assert.deepStrictEqual(nameAndAge(), expectedResult2);
+
+  
+
    
